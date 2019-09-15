0de4482bd7bcaade04fc3b4c7addbb94f064048b
Hussain Mansoor <hussain.mansoor@rea-group.com>
2019-09-15 15:36:03 +0800

    Fixed the sound playing on shake

--- DummyApp2/app/src/main/java/solutions/manshur/dummyapp2/MainActivity.java
+++ DummyApp2/app/src/main/java/solutions/manshur/dummyapp2/MainActivity.java
@@ -1,19 +1,12 @@
 package solutions.manshur.dummyapp2;
 
-import solutions.manshur.dummyapp2.ShakeDetector.OnShakeListener;
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,26 +21,46 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        // Initialize media player
+        mMediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.sound);
+
+        // Add OnCompletionListener to release the
+        mMediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+//                mMediaPlayer.reset();
+//                mMediaPlayer.release();
+            }
+        });
+
+        Toast.makeText(MainActivity.this, "shake", Toast.LENGTH_SHORT).show();
+
+        mMediaPlayer.start();
+
         // ShakeDetector initialization
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         mShakeDetector = new ShakeDetector();
 
         mShakeDetector.setOnShakeListener(new ShakeDetector.OnShakeListener() {
+
             public void onShake() {
-                // Initialize media player
-                mMediaPlayer = MediaPlayer.create(MainActivity.this,R.raw.sound);
 
-                // Add OnCompletionListener to release the
-                mMediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-                    @Override
-                    public void onCompletion(MediaPlayer mp) {
-                        mMediaPlayer.release();
-                    }
-                });
+                Toast.makeText(MainActivity.this, "shake", Toast.LENGTH_SHORT).show();
 
-                mMediaPlayer.start();
+
+                try {
+                    mMediaPlayer.start();
+                } catch (IllegalStateException e) {
+                    e.printStackTrace();
+                }
+
             }//onShake()
         });//setOnShakeListener()
+
+        mSensorManager.registerListener(mShakeDetector,
+                mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                SensorManager.SENSOR_DELAY_UI);
 
     }
 
@@ -55,7 +68,7 @@ public class MainActivity extends AppCompatActivity {
         super.onResume();
         mSensorManager.registerListener(mShakeDetector,
                 mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
-                mSensorManager.SENSOR_DELAY_UI);
+                SensorManager.SENSOR_DELAY_UI);
     }
 
     protected void onPause() {
